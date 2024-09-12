b46c424dfb3fbfc2583c982f17bf9468b08bfa24
rjrocha19 <rodolfo_rj@live.com>
2024-09-12 19:13:58 -0300

    update

--- src/functions/get-week-summary.ts
+++ src/functions/get-week-summary.ts
@@ -1,9 +1,10 @@
-import { lte } from "drizzle-orm";
+import { count, gte, lte, sql } from "drizzle-orm";
 import { db } from "../db";
-import { goals } from "../db/schema";
+import { goals, goalsCompletions } from "../db/schema";
 import dayjs from "dayjs";
 
 export async function getWeekSummary() {
+  const firstDayOfWeek = dayjs().startOf("week").toDate();
   const lastDayOfWeek = dayjs().endOf("week").toDate();
 
   const goalsCreatedUpToWeek = db.$with("goals_created_up_to_week").as(
@@ -17,6 +18,50 @@ export async function getWeekSummary() {
       .from(goals)
       .where(lte(goals.createdAt, lastDayOfWeek))
   );
+
+  const goalsCompletedInWeek = db.$with("goals_completed_in_week").as(
+    db
+      .select({
+        id: goalsCompletions.id,
+        title: goals.title,
+        completedAt: goalsCompletions.createdAt,
+        completedAtDate: sql/*sql*/`
+          DATE(${goalsCompletions.createdAt})
+        `.as("completedAtDate"),
+      })
+      .from(goalsCompletions)
+      .innerJoin(goals, eq(goals.id, goalsCompletions.goalId))
+      .where(
+        and(
+          gte(goalsCompletions.createdAt, firstDayOfWeek),
+          lte(goalsCompletions.createdAt, lastDayOfWeek)
+        )
+      )
+  );
+
+  const goalsCompletedByWeek = db.$with("goals_completed_by_week").as(
+    db
+      .select({
+        completedAtDate: goalsCompletedInWeek.completedAtDate,
+        completions: sql/*sql*/`
+         JSON_AGG(
+          JSON_BUILD_OBJECT(
+            'id', ${goalsCompletedInWeek.id},
+            'title', ${goalsCompletedInWeek.title},
+            'completedAt', ${goalsCompletedInWeek.completedAt}
+          )
+         )
+        `.as("completions"),
+      })
+      .from(goalsCompletedInWeek)
+      .groupBy(goalsCompletedInWeek.completedAtDate)
+  )
+
+  const result = await db
+    .with(goalsCreatedUpToWeek, goalsCompletedByWeek)
+    .select()
+    .from(goalsCreatedUpToWeek)
+    
 
   return (
     summary: 'test',
